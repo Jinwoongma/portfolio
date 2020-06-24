ae4fac4b6a178ed58508c72c411a71fb6bc936a1
Jinwoong <jinwoongma@gmail.com>
2020-06-24 12:02:17 +0900

    JS: project animation

--- main.js
+++ main.js
@@ -52,6 +52,27 @@ arrowUp.addEventListener('click', () => {
 })
 
 
+// Project
+const workBtnContainer = document.querySelector('.work__categories')
+const projectContainer = document.querySelector('.work__projects')
+const projects = document.querySelectorAll('.project')
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add('animate-out')
+    setTimeout(() => {
+        projects.forEach(project => {
+            if (filter === '*' || filter == project.dataset.type) {
+                project.classList.remove('invisible')
+            } else {
+                project.classList.add('invisible')
+            }
+        })
+        projectContainer.classList.remove('animate-out')
+    }, 300)
+})
 
 
 function scrollIntoView(selector) {
